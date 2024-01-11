f7ea473706ead1f2b985a0d6e9665117b198dfa2
GAMMER <ladkegitesh20@gmail.com>
2024-01-11 13:46:16 +0530

    commita

--- src/Errorpage.jsx
+++ src/Errorpage.jsx
@@ -5,6 +5,7 @@ const Errorpage = () => {
     <>
     <div className='FP'>
         <h1>404 Page Not Found</h1>
+        <p>The page you are looking for could not be found. It may have been moved <p/>
        <p>Page Looking for is not present.....</p>
     </div>
        
